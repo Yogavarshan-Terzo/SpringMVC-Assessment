285cb90a0659a9f54cede2a4d728f434b8a2a229
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-07-04 13:37:57 +0530

    User DetailsService

--- src/main/java/com/assessment/terzoemployees/service/impl/UserServiceImpl.java
+++ src/main/java/com/assessment/terzoemployees/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.assessment.terzoemployees.repository.RoleRepository;
 import com.assessment.terzoemployees.repository.UserRepository;
 import com.assessment.terzoemployees.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -15,19 +16,23 @@ import java.util.Arrays;
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
-
+    private PasswordEncoder passwordEncoder;
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+
+
 
     @Override
     public void saveUser(RegistrationDto registrationDto) {
         UserEntity user = new UserEntity();
         user.setUsername(registrationDto.getUsername());
         user.setEmail(registrationDto.getEmail());
-        user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
